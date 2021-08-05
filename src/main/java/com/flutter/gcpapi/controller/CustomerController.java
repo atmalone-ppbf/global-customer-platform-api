57f35889576ea815af099fb990d0b7ab523e635b
Andrew Malone <andrew.malone@paddypowerbetfair.com>
2021-08-05 18:28:04 +0100

    added new service for nicknam

--- src/main/java/com/flutter/gcpapi/controller/CustomerController.java
+++ src/main/java/com/flutter/gcpapi/controller/CustomerController.java
@@ -5,12 +5,16 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.flutter.gcpapi.model.Customer;
 import com.flutter.gcpapi.model.NicknamedAccount;
 import com.flutter.gcpapi.service.CustomerService;
+import com.flutter.gcpapi.service.NicknameService;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -26,38 +30,42 @@ public class CustomerController {
     @GetMapping("/inspect")
     public String inspect(
             @RequestParam(name = "searchString") String searchString,
-            @RequestParam(name = "brandId") String brandId
+            @RequestParam(name = "brand") String brand
     ) throws Exception {
-
+        String customerJson = "customer not found";
         try {
-
-            String key = "";
-            boolean isAccountIdSearch = false;
+            String key;
+            boolean isNicknameSearch = false;
 
             //Determine if search string is nickname or customer id
             //Create the key
-            if(NumberUtils.isCreatable(searchString)){
-                Double accountId = Double.valueOf(searchString);
-                isAccountIdSearch = true;
-                key = brandId + accountId;
-            }else{
-                key = brandId + searchString;
+            if (NumberUtils.isCreatable(searchString)) {
+                Double accountIdFromSearch = Double.valueOf(searchString);
+                key = brand + accountIdFromSearch;
+            } else {
+                isNicknameSearch = true;
+                key = brand + searchString;
             }
 
-
-            CustomerService service = CustomerService.init("localhost", 8080);
-            if (isAccountIdSearch) {
-                //if the key is a Brand+accountId call service for getting the state of CUSTOMER_STATE_DESCRIPTOR
-                final Customer customer = service.queryCustomerState(key);
-            }
-            else {
+            if (isNicknameSearch) {
+                //The search is for an account with brand+Nickname
                 //Call customer nickname service to get NICKNAMED_ACCOUNT_STATE_DESCRIPTOR then get customer from the matched account.
-                final NicknamedAccount nicknamedAccount = service.queryNicknamedAccountState(key);
+                final NicknameService nicknameService = NicknameService.init("localhost", 9069);
+                //Get the nicknamed account from state
+                final Map<String, NicknamedAccount> nicknamedAccountMap = nicknameService.queryNicknamedAccountState(key);
+
+                NicknamedAccount nicknamedAccount = nicknamedAccountMap.get(searchString);
+                key = nicknamedAccount.getBrand() + nicknamedAccount.getAccountId();
             }
+
+            //get the customer account from the nicknamed account
+            CustomerService service = CustomerService.init("localhost", 8080);
+            final Customer customer = service.queryCustomerState(key);
+            Gson gson = new Gson();
+            customerJson = gson.toJson(customer);
         } catch (Exception e) {
             log.error("Exception thrown", e);
         }
-
-        return "state";
+        return customerJson;
     }
 }
--- src/main/java/com/flutter/gcpapi/service/NicknameService.java
+++ src/main/java/com/flutter/gcpapi/service/NicknameService.java
@@ -1,15 +1,12 @@
 package com.flutter.gcpapi.service;
 
-import com.flutter.gcpapi.model.*;
+import com.flutter.gcpapi.model.NicknamedAccount;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.JobID;
 import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.queryablestate.client.QueryableStateClient;
 
@@ -19,15 +16,9 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Slf4j
-public class CustomerService {
+public class NicknameService {
 
     private final QueryableStateClient client;
-
-    private static final ValueStateDescriptor<Customer> customerStateDescriptor =
-            new ValueStateDescriptor<>(
-                    "customerState",
-                    Customer.class
-            );
 
     private static final MapStateDescriptor<String, NicknamedAccount> nicknamedAccountStateDescriptor =
             new MapStateDescriptor<>(
@@ -36,14 +27,10 @@ public class CustomerService {
                     Types.POJO(NicknamedAccount.class)
             );
 
-    private CustomerService(String host, Integer port) throws UnknownHostException {
+    private NicknameService(String host, Integer port) throws UnknownHostException {
         log.info("Initiating connecting with {}:{}", host, port);
         this.client = new QueryableStateClient(host, port);
         client.setExecutionConfig(new ExecutionConfig());
-    }
-
-    public Customer queryCustomerState(String key) throws Exception {
-        return client.getKvState(JobID.fromHexString("CHANGETHISMOFO"), "QueryableCustomerState", key, BasicTypeInfo.STRING_TYPE_INFO, customerStateDescriptor).get().value();
     }
 
     public Map<String,NicknamedAccount> queryNicknamedAccountState(String key) throws Exception {
@@ -66,7 +53,7 @@ public class CustomerService {
     }
 
 
-    public static CustomerService init(String host, Integer port) throws UnknownHostException {
-        return new CustomerService(host, port);
+    public static NicknameService init(String host, Integer port) throws UnknownHostException {
+        return new NicknameService(host, port);
     }
 }
