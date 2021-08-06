efaed4ffa941693d275eaa18745479f526bd1d1e
Andrew Malone <andrew.malone@paddypowerbetfair.com>
2021-08-06 12:13:45 +0100

    Update GcpApiApplication.java
    updated application

---
+++ src/main/java/com/flutter/gcpapi/GcpApiApplication.java
@@ -0,0 +1,13 @@
+package com.flutter.gcpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpApiApplication.class, args);
+	}
+
+}
